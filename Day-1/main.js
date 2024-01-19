b2c2195cf7c7b147315e9cf43dde0b20d9d40b37
Ayush-1206 <ayushbankhede12gmail.com>
2024-01-19 12:39:12 +0530

    git setup

---
+++ Day-1/main.js
@@ -0,0 +1,9 @@
+// const clickMebtn = document.getElementById("click-me-id");
+// clickMebtn.addEventListener("click", () => {
+//   alert("Hey");
+// });
+
+const heading = document.createElement("h1");
+heading.innerHTML = "Hello People!";
+const root = document.getElementById("root");
+root.appendChild(temp);
